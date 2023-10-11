355c22f39867f41db75eb0a32381da01f239ae7a
LouisLin511986 <93067887+LouisLin511986@users.noreply.github.com>
2023-10-11 15:24:40 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8NBssJ1NvzRCKhqAtj2EFPrWNsCKLDWU",
-    authDomain: "stories-firebase-app-8ef76.firebaseapp.com",
-    projectId: "stories-firebase-app-8ef76",
-    storageBucket: "stories-firebase-app-8ef76.appspot.com",
-    messagingSenderId: "764755795941",
-    appId: "1:764755795941:web:d8a07418ed060951ecd90c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
